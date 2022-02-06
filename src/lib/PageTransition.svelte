82841cce5a36edc342546cf0e9a4bff27d49782d
Maxime Quandalle <maxime@quandalle.com>
2022-02-06 14:54:05 +0100

    demo app

---
+++ src/lib/PageTransition.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { fly } from "svelte/transition";
+  import { page } from "$app/stores";
+
+  let duration = 3000;
+  let transitionX = 700;
+</script>
+
+<div style:display="grid" style:overflow="hidden">
+  {#key $page.url.pathname}
+    <div
+      in:fly={{ x: transitionX, duration: duration }}
+      out:fly|local={{ x: -transitionX, duration }}
+      style:grid-column="1 / 1"
+      style:grid-row="1 / 1"
+    >
+      <slot />
+    </div>
+  {/key}
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import PageTransition from "$lib/PageTransition.svelte";
+</script>
+
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/contact">Contact</a></li>
+</ul>
+
+<PageTransition>
+  <div class="card">
+    <slot />
+  </div>
+</PageTransition>
+
+<style>
+  .card {
+    border: 1px solid grey;
+    padding: 40px;
+    max-width: 600px;
+  }
+</style>
